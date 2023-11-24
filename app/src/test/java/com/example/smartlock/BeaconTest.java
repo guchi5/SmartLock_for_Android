5bd581c52bdfafe2740d6a5904caf22f65377c5b
guchi5 <zero19390901@gmail.com>
2023-11-24 10:21:14 +0900

    テスト用のクラスを追加

--- app/src/test/java/com/example/smartlock/BeaconTest.java
+++ app/src/test/java/com/example/smartlock/BeaconTest.java
@@ -1,4 +1,31 @@
 package com.example.smartlock;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import org.junit.Test;
+
+import java.util.Date;
 
 public class BeaconTest {
+
+    @Test
+    public void getUUID(){
+        Beacon beacon = new Beacon("test", 1, 2);
+        assertEquals("test", beacon.getUUID());
+    }
+    public void getMajor(){
+        Beacon beacon = new Beacon("test", 1, 2);
+        assertEquals(1, beacon.getUUID());
+    }
+    public void getMinor(){
+        Beacon beacon = new Beacon("test", 1, 2);
+        assertEquals(2, beacon.getUUID());
+    }
+
+    public void getDate(){
+        Beacon beacon = new Beacon("test", 1, 2);
+        beacon.setDate(new Date());
+        assertEquals(2, beacon.getDate());
+    }
+
 }
--- app/src/test/java/com/example/smartlock/SmartLockTest.java
+++ app/src/test/java/com/example/smartlock/SmartLockTest.java
@@ -1,2 +1,25 @@
-package com.example.smartlock;public class SmartLockTest {
+package com.example.smartlock;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class SmartLockTest {
+    @Test
+    public void getUUID(){
+        SmartLock smartLock = new SmartLock();
+        assertEquals(null, smartLock.getUUID());
+    }
+
+    @Test
+    public void getSecretKey(){
+        SmartLock smartLock = new SmartLock();
+        assertEquals(null, smartLock.getSecretKey());
+    }
+
+    @Test
+    public void getName(){
+        SmartLock smartLock = new SmartLock();
+        assertEquals(null, smartLock.getName());
+    }
 }
